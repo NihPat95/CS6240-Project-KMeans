e7fd2b4c0828b0a290b18468ad7237c10a7ca8c6
Jatin Sadhwani <jatinsadhwani24@gmail.com>
2019-03-19 17:33:52 -0400

    Project setup

---
+++ src/main/java/kmeans/KMeans.java
@@ -0,0 +1,21 @@
+package kmeans;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class KMeans extends Configured implements Tool {
+}
