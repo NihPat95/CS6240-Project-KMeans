8ea0652f9d187a80434b402cec8252539213ff12
Jatin Sadhwani <jatinsadhwani24@gmail.com>
2019-03-29 00:24:05 -0400

    Calculated initial k clusters

--- src/main/java/kmeans/KMeans.java
+++ src/main/java/kmeans/KMeans.java
@@ -13,6 +13,10 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class KMeans extends Configured implements Tool {
 
     private static final Logger logger = LogManager.getLogger(KMeans.class);
@@ -27,6 +31,28 @@ public class KMeans extends Configured implements Tool {
         # TODO loop the join in some iteration or till converge
         # TODO read and redistribute the c points from the previous iteration
          */
+
+        //k is the number of clusters
+        //d is vector dimension, in this case, number of movies
+        double[][] initial_k = new double[k][d];
+
+        File file = new File("path/to/file");
+        file.createNewFile();
+        BufferedWriter buffer_writer = new BufferedWriter(new FileWriter(file.getAbsoluteFile(), true));
+        String data;
+        //Calculating initial k points randomly and writing them to a file
+        for(int i=0; i< k; i++){
+            //generates a random rating from 0.0 to 5.0
+            data = Integer.toString(i);
+            buffer_writer.write(data);
+            for(int j = 0; j < d; j++){
+                double random = 0.0 + Math.random() * (5.0 - 1.0);
+                k[i][j] = random;
+                data = "," + Double.toString(k[i][j]);
+                buffer_writer.write(data);
+            }
+        }
+
 
         final Configuration conf = getConf();
         final Job job = Job.getInstance(conf, "Kmeans");
