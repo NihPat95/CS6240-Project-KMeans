7210f34b1c2d7fa515b1689abdad206075fa5b2b
Jatin Sadhwani <jatinsadhwani24@gmail.com>
2019-03-29 04:06:44 -0400

    Reducer class for calculating new distances done

--- src/main/java/kmeans/ReduceTask.java
+++ src/main/java/kmeans/ReduceTask.java
@@ -1,8 +1,6 @@
 package kmeans;
 
-import org.apache.hadoop.io.ArrayWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
@@ -10,8 +8,25 @@ import java.io.IOException;
 public class ReduceTask extends Reducer <IntWritable, ArrayWritable, IntWritable, Text> {
     @Override
     protected void reduce(IntWritable key, Iterable<ArrayWritable> values, Context context) throws IOException, InterruptedException {
-        /*
-        # TODO for each key take the average of the points and emit
-         */
+        int size = (int)values.spliterator().getExactSizeIfKnown();
+        double[] sum = new double[size];
+
+        //For each value in the collection of distances
+        for(ArrayWritable value : values){
+           int i = 0;
+           //Addition of each feature distance
+           for(Writable writable : value.get()) {
+               DoubleWritable doubleWritable = (DoubleWritable)writable;
+               sum[i] += doubleWritable.get();
+               i++;
+           }
+        }
+
+        //Calculates the average of all the distances
+        for(int i = 0;i<sum.length;i++){
+            sum[i] = sum[i]/size;
+        }
+
+        //# TODO : Convert the value into Text and emit it
     }
 }
