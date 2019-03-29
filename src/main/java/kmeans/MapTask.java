57f345399c42df115162ce6b4d8629e8c60d2204
Jatin Sadhwani <jatinsadhwani24@gmail.com>
2019-03-29 02:11:41 -0400

    Mapper function to calculate distance of points with centres complete

--- src/main/java/kmeans/MapTask.java
+++ src/main/java/kmeans/MapTask.java
@@ -11,9 +11,8 @@ import java.io.IOException;
 import java.net.URI;
 
 public class MapTask extends Mapper<Object, Text, IntWritable, ArrayWritable> {
-    @Override
-
     public static double[][] C;
+    @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         super.setup(context);
 
@@ -48,19 +47,46 @@ public class MapTask extends Mapper<Object, Text, IntWritable, ArrayWritable> {
 
     @Override
     protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        /*
-        # TODO compare each point with points in C and emit the index of minimum with the point value
-         */
-
         super.map(key, value, context);
+        String[] userRatings = value.toString().split(",");
+
+        //Getting the user ID and ratings for that user from data
+        int userId = Integer.parseInt(userRatings[0]);
+        double[] ratings = new double[userRatings.length - 1];
+        for(int i = 1;i<=userRatings.length;i++){
+            ratings[i-1] = Double.parseDouble(userRatings[i]);
+        }
+
+        double min_distance = Double.MAX_VALUE;
+        double min_index = 0;
+
+        //Calculating distance of point with each cluster centre, i.e, each C value
+        for(int i = 0; i<C.length;i++){
+            double euc_distance = this.computeEuclideanDistance(C[i], ratings);
+            if(euc_distance < min_distance){
+                min_distance = euc_distance;
+                min_index = i;
+            }
+        }
+
+        //Return value of minimum C as key and its user Id as value
+        // # TODO : Change it later to satisfy hadoop data types
+        context.write(min_index, userId);
+
     }
 
 
-    private double computeEuclideanDistance(int x[], int y[]){
-        /*c
-        # TODO return the euclidean distance between two points, dimension of x y must match
-         */
-        return 0;
+    private double computeEuclideanDistance(double x[], double y[]){
+        double distance = 0;
+        if(x.length != y.length){
+            System.out.print("DIMENSIONS MUST MATCH");
+        }
+
+        //Computes Euclidean Distance of two vectors
+        for(int i = 0; i<x.length; i++){
+            distance += Math.pow(x[i] - y[i], 2);
+        }
+        return Math.sqrt(distance);
     }
 
 }
